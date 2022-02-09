1e930c86874d0a131387de12c51fcde55e59d1df
Ogelo10 <augustineofoegbu1@gmail.com>
2022-02-09 12:26:17 +0100

    React Crypto Tracker

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -5,7 +5,7 @@ const Coin = ({ name, icon, price, symbol }) => {
     <div className="coin">
       <h1>Name: {name}</h1>
       <h3>Price: {price}</h3>
-      <img src={icon} alt={`$name`} />
+      <img src={icon} alt={` ${name} `} />
       <h3>Symbol: {symbol}</h3>
     </div>
   );
